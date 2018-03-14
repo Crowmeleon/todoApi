5e9a1d54a34b939f19f9e7a8904a71f45ab4eda3
petermartinez225@gmail.com <peter@thriftmatch.com>
2018-03-13 22:04:27 -0700

    add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -89,6 +90,19 @@ app.delete('/todos/:id', (req,res) => {
         res.send({todo});
     }).catch(e => res.status(400).send());
 });
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then(token => {
+        res.header('x-auth', token).send(user)
+    }).catch(err => {
+        res.status(400).send();
+    });
+})
 
 app.listen(port, () => {
     console.log(`Application started at ${port}`);
