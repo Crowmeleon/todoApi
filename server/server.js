d0880bf1ef537216d9cc2a05160c5a09d2c7a74f
petermartinez225@gmail.com <peter@thriftmatch.com>
2018-04-08 23:46:09 -0700

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const jwt = require('jsonwebtoken');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT;
@@ -102,7 +103,11 @@ app.post('/users', (req, res) => {
     }).catch(err => {
         res.status(400).send();
     });
-})
+});
+
+app.get('/users/me', authenticate, (req, res)=> {
+    res.send(req.user);
+});
 
 app.listen(port, () => {
     console.log(`Application started at ${port}`);
